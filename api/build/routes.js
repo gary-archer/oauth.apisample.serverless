84db96a357cd535d65bd9babeb77b4fb65d6b876
gary <gary@MACSTATION.local>
2018-10-29 22:45:17 +0000

    Refinements

---
+++ api/build/routes.js
@@ -0,0 +1,45 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const middy_1 = __importDefault(require("middy"));
+/*
+ * Define lambda routes
+ */
+class Routes {
+    getCompanyList(event, context) {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('In getCompanyList');
+            return {
+                result: 'success',
+                message: 'company list',
+            };
+        });
+    }
+    getCompanyTransactions(event, context) {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('In getCompanyTransactions');
+            return {
+                result: 'success',
+                message: 'company transactions',
+            };
+        });
+    }
+}
+/*
+ * Plug in middleware
+ */
+const routes = new Routes();
+const getCompanyList = middy_1.default(routes.getCompanyList);
+exports.getCompanyList = getCompanyList;
+const getCompanyTransactions = middy_1.default(routes.getCompanyTransactions);
+exports.getCompanyTransactions = getCompanyTransactions;
