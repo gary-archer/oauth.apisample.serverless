23a275444a74fd73eb44a9207a9275456c42e389
gary <gary@MACSTATION.local>
2020-07-05 20:33:00 +0100

    Updates

--- src/host/claims/sampleApiClaimsProvider.ts
+++ src/host/claims/sampleApiClaimsProvider.ts
@@ -7,21 +7,26 @@ import {SampleApiClaims} from '../claims/sampleApiClaims';
 export class SampleApiClaimsProvider implements CustomClaimsProvider<SampleApiClaims> {
 
     /*
-     * Add details from the API's own database
+     * Add custom claims that cannot be provided by the Authorization Server
      */
     public async addCustomClaims(accessToken: string, claims: SampleApiClaims): Promise<void> {
 
         // Look up the user id in the API's own database
         this._lookupDatabaseUserId(claims);
 
-        // Look up the user id in the API's own data
+        // Look up authorization rules in the API's own data
         this._lookupAuthorizationData(claims);
     }
 
     /*
-     * A real implementation would get the subject / email claims and find a match in the API's own data
+     * A real implementation would look up a user in the API's database
+     * This might involve a SQL filter on the token's Subject claim or the user info's Email claim
+     * 'SELECT * from USERS where OAUTH_ID=<token subject>'
+     * 'SELECT * from USERS where EMAIL=<user info email>'
      */
     private _lookupDatabaseUserId(claims: SampleApiClaims): void {
+
+        // In the API's own data this is likely to be a database primary key
         claims.userDatabaseId = '10345';
     }
 
@@ -31,7 +36,7 @@ export class SampleApiClaimsProvider implements CustomClaimsProvider<SampleApiCl
      */
     private _lookupAuthorizationData(claims: SampleApiClaims): void {
 
-        // We use a coverage based authorization rule where the user can only use data for these regions
+        // We use a coverage based authorization rule where the user can only see data for these regions
         claims.regionsCovered = ['Europe', 'USA'];
     }
 }
