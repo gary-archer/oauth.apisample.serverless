0137ce01f1df016ef1ae313688ff423c09c8dbc7
gary <garyarcher36@gmail.com>
2023-03-20 18:25:29 +0000

    Updates

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#################################
-# A script to run lambdas locally
-#################################
+##########################################################################
+# A script to run lambdas locally, from login to logout
+# On Windows, ensure that you have first set Git bash as the node.js shell
+# npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
+##########################################################################
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
