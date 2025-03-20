f87312494a3a8bb8a6db3ac0d61ae44837776a01
gary-archer <garyarcher36@gmail.com>
2025-03-20 08:17:16 +0000

    Updates

--- src/plumbing/middleware/authorizerMiddleware.ts
+++ src/plumbing/middleware/authorizerMiddleware.ts
@@ -3,7 +3,6 @@ import {APIGatewayProxyResult} from 'aws-lambda';
 import {ClaimsPrincipal} from '../claims/claimsPrincipal.js';
 import {ClaimsReader} from '../claims/claimsReader.js';
 import {BASETYPES} from '../dependencies/baseTypes.js';
-import {ClientError} from '../errors/clientError.js';
 import {LogEntryImpl} from '../logging/logEntryImpl.js';
 import {OAuthFilter} from '../oauth/oauthFilter.js';
 import {APIGatewayProxyExtendedEvent} from '../utilities/apiGatewayExtendedProxyEvent.js';
@@ -21,30 +20,15 @@ export class AuthorizerMiddleware implements middy.MiddlewareObj<APIGatewayProxy
         // Get the log entry for this request
         const logEntry = request.event.container.get<LogEntryImpl>(BASETYPES.LogEntry);
 
-        try {
+        // Get the filter and call it to do the work and return claims
+        const filter =  request.event.container.get<OAuthFilter>(BASETYPES.OAuthFilter);
+        const claimsPrincipal = await filter.execute(request.event);
 
-            // Get the filter and call it to do the work and return claims
-            const filter =  request.event.container.get<OAuthFilter>(BASETYPES.OAuthFilter);
-            const claimsPrincipal = await filter.execute(request.event);
+        // Include identity details in logs as soon as we have them
+        logEntry.setIdentity(ClaimsReader.getStringClaim(claimsPrincipal.jwt, 'sub'));
 
-            // Include identity details in logs as soon as we have them
-            logEntry.setIdentity(ClaimsReader.getStringClaim(claimsPrincipal.jwt, 'sub'));
-
-            // Bind it to make claims injectable
-            request.event.container.bind<ClaimsPrincipal>(BASETYPES.ClaimsPrincipal).toConstantValue(claimsPrincipal);
-
-        } catch (e) {
-
-            // Log the cause of any 401 errors
-            if (e instanceof ClientError) {
-
-                logEntry.setClientError(e);
-                logEntry.setResponseStatus(401);
-            }
-
-            // Rethrow the error
-            throw e;
-        }
+        // Bind it to make claims injectable
+        request.event.container.bind<ClaimsPrincipal>(BASETYPES.ClaimsPrincipal).toConstantValue(claimsPrincipal);
 
         // For async middleware, middy calls next for us, so do not call it here
     }
