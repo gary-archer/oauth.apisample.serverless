a0a9a548768eb3343b6a905d1b8a558b10bc480c
gary <garyarcher36@gmail.com>
2025-11-27 09:43:17 +0000

    Updates

--- run_api.sh
+++ run_api.sh
@@ -7,6 +7,20 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #
+# A utility to change the Node.js version that Serverless Offline receives
+#
+function replaceServerlessVersion() {
+
+  local _FROM='nodejs24.x';
+  local _TO='nodejs22.x';
+  if [ "$(uname -s)" == 'Darwin' ]; then
+    sed -i '' "s/$_FROM/$_TO/" ./serverlessOffline.yml
+  else
+    sed -i "s/$_FROM/$_TO/" ./serverlessOffline.yml
+  fi
+}
+
+#
 # Install dependencies if needed
 #
 npm install
@@ -37,11 +51,20 @@ if [ $? -ne 0 ]; then
 fi
 
 #
+# Make a replacement until Serverless Offline supports Node.js 24
+#
+rm serverlessOffline.yml 2>/dev/null
+cp serverless.yml serverlessOffline.yml
+replaceServerlessVersion
+
+#
 # Run Serverless offline to expose lambda endpoints at HTTPS endpoints
 #
 npx sls offline \
+  --config ./serverlessOffline.yml \
   --useInProcess \
   --noPrependStageInUrl \
+  --noSponsor \
   --prefix 'investments' \
   --host '0.0.0.0' \
   --httpPort 446 \
