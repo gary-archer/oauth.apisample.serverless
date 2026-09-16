bae29ca757649130263102f8c09df0bacd59ac36
gary-archer <garyarcher36@gmail.com>
2026-09-16 13:51:35 +0100

    Updates

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -1,9 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import fs from 'fs';
 import {builtinModules} from 'module';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import {RollupOptions} from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
 
